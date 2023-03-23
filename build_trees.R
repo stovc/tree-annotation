3a70da91cfc4d0614860960b1e87a3f4f6a51d09
Stepan Ovchinnikov <stovch@gmail.com>
2023-03-23 12:48:53 +0100

    updated sample analysis

--- build_trees.R
+++ build_trees.R
@@ -37,19 +37,14 @@ org_tree_full <- read.tree(file="org_trees/org_tree_full.nwk")
 org_data_full <- read_delim("org_trees/org_tree_full_data.csv", delim=';')
 
 org_tree_genus <- read.tree(file="org_trees/org_tree_genus.nwk")
-org_data_genus <- read_delim("org_trees/org_tree_genus_data.csv", delim=';')
 
 org_tree_family <- read.tree(file="org_trees/org_tree_family.nwk")
-org_data_family <- read_delim("org_trees/org_tree_family_data.csv", delim=';')
 
 org_tree_order <- read.tree(file="org_trees/org_tree_order.nwk")
-org_data_order <- read_delim("org_trees/org_tree_order_data.csv", delim=';')
 
 org_tree_class <- read.tree(file="org_trees/org_tree_class.nwk")
-org_data_class <- read_delim("org_trees/org_tree_class_data.csv", delim=';')
 
 org_tree_phylum <- read.tree(file="org_trees/org_tree_phylum.nwk")
-org_data_phylum <- read_delim("org_trees/org_tree_phylum_data.csv", delim=';')
 
 ######################
 #      ANNOTATE
@@ -58,15 +53,15 @@ org_data_phylum <- read_delim("org_trees/org_tree_phylum_data.csv", delim=';')
 tree_all <- annotate_tree(
   tree, 
   start='all', 
-  expand=c('Proteobacteria'), 
+  expand=c('Pseudomonadota'), 
   threshold=1)
 
 # Tree Actinobacteria
 tree_act <- annotate_tree(
   tree, 
-  start = 'Actinobacteria', 
-  expand = c('Actinobacteria', 'Actinomycetia', 'Corynebacteriales'), 
-  threshold = 1)
+  start = 'Actinomycetota', 
+  expand = c('Actinomycetota', 'Actinomycetes', 'Corynebacteriales')
+  )
 
 # Tree Cyanobacteria
 tree_cyan <- annotate_tree(
@@ -86,42 +81,15 @@ paralog_df = data.frame(
   paralog = c("ClpC", "ClpC2", "ClpC3", "ClpCB", "ClpB")
 )
 
-# add the paralog data on the annotated tree. 2nd argument stands for the basic paralog name
-# annotated tree is needed to make a list of paralogs
 tree_par <- assign_paralogs(tree_all, "ClpCB", paralog_df)
-tab_tree_par = as_tibble(tree_par) # and get it in the tabular form
 
-# form the list of paralog values of the clustered proteins and their taxid numbers to annotate the species tree
-paralogs = tab_tree_par[!is.na(tab_tree_par$taxid), c('label', 'taxid', 'paralog')]
-paralogs$representative = paralogs$label
+# ORGANISM TREE
 
-# paralogs + cluster data -> all_paralogs
-cluster_data2 <- full_join(cluster_data, paralogs, by='representative')
-cluster_data2 <- mutate(cluster_data2, id = ifelse(is.na(id), representative, id)) # why can id be NA???
-cluster_data2 <- cluster_data2[!duplicated(cluster_data2$id),]  # where do duplicates come from?
-cluster_data2 <- cluster_data2[, c('id', 'paralog')]
-names(cluster_data2)[1] <- 'ID'
-all_paralogs <- full_join(filtered_data, cluster_data2, by='ID')
-all_paralogs <- all_paralogs[, c('assembly', 'taxid', 'paralog')]
-
-counted <- all_paralogs %>% group_by(assembly, paralog) %>% mutate(count=n())
-counted <- distinct(counted)
-counted <- pivot_wider(counted, names_from = 'paralog', values_from = 'count')
-
-counted[is.na(counted)] <- 0
-
-counted2 <- subset(counted, select = -c(assembly))
-# apparently, I summurize for the case when I have several asssemblies for the same genome (should I better use median?)
-counted2 <- counted2 %>% group_by(taxid) %>% summarize_all(mean)
-colnames(counted2)[1] <- 'label'
-counted2$label = as.character(counted2$label)
-
-# 
-
-org_tree_full_a = annotate_org_tree(org_tree_full, org_data_full, counted2)
+org_tree_full_a = annotate_org_tree(org_tree_full, org_data_full, tree_par)
 
 org_tree_family_a = copy_annotation(org_tree_family, org_tree_full_a)
 org_tree_phylum_a = copy_annotation(org_tree_phylum, org_tree_full_a)
+org_tree_order_a = copy_annotation(org_tree_order, org_tree_full_a)
 
 ######################
 #        PLOT
@@ -130,7 +98,7 @@ org_tree_phylum_a = copy_annotation(org_tree_phylum, org_tree_full_a)
 # BASIC
 # basic with bootstraps
 plot_tree(tree_all, "circular", "branch.length", tips="product", circle="bootstrap",
-          filename="tree-base.pdf", width=150, height=100)
+          filename="tree-base.pdf", width=150, height=100, collapse = T)
 # length
 plot_tree(tree_all, "circular", "branch.length", tips="product", circle="length",
           filename="tree-base-len.pdf", width=150, height=100)
@@ -141,9 +109,18 @@ plot_tree(tree_all, "equal_angle", "branch.length", context=NA,
 plot_tree(tree_par, "circular", "branch.length", aes_color="paralog", 
           width=150, height=100, filename="tree-paralogs.pdf")
 
+# basic Act
+plot_tree(tree_act, "circular", "branch.length", tips="product", circle="bootstrap",
+          filename="tree-base-act.pdf", width=150, height=100, collapse = "Actinomycetota")
+
 # TAXALINK
+plot_tree(tree_act, "inward_circular", "none", taxalink=T, label_nodes=T, circle="bootstrap",
+          width=50, height=33, filename="tree-taxalink-act.svg", collapse = "Actinomycetota")
+
+# taxalink Act
 plot_tree(tree_all, "inward_circular", "none", taxalink=T, label_nodes=T, collapse=NA, circle="bootstrap",
-          width=50, height=33, filename="tree-taxalink-all.svg")
+          width=50, height=33, filename="tree-taxalink-act.svg")
+
 # taxalink len
 plot_tree(tree_all, "inward_circular", "none", taxalink=T, label_nodes=T, collapse=NA, circle="length",
           width=50, height=33, filename="tree-taxalink-all-len.svg")
